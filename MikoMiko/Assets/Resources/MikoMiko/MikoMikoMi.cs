c6968fedee49976e3be3ff174b34cf3feef58ee8
KizunaAIchan <1309792100@qq.com>
2020-05-19 13:03:13 +0800

    Mikooooooooooooooooooo!

---
+++ MikoMiko/Assets/Resources/MikoMiko/MikoMikoMi.cs
@@ -0,0 +1,66 @@
+﻿
+
+/*            Font Type: Consolas                                                                                      
+                                                                                                    
+                                                                             `.|~|.                         
+                                                                           -i+>,.`,~!|        `             
+              #   #    #   #  #    ##                                     `7``.,|~_!s"~:^^.}7s[v|           
+             # # # #   #   ##     #  #                                   ^"i]ytsrrrryyo77=o*4**bk=          
+            #   #   #  #   #  #    ##                                   }xx?p*Ux?zo7?t]?uuxuEDObwe}`        
+                                                                       ixCeobh2sSnxoxxzxzSxozfP2xSb[,       
+                                                                      ,*2Z(zSPx|-)oSSpwkh4pZ27xLZ[pfy,      
+                                                                      .SGw_|!4o,"S&DGZfOPOp2boxi-s?%Uy`     
+                                                                       kPsPE&+|,,|++_|,(e)li=7ee^>7PnYx;    
+                                                                      .em4!!;,,,,,,,,,:-ii(o?r*Z;(*Ge`+o;   
+                                                                      .i+f|;,,+",,,:|_(Z4wuz]7w*"ifl`       
+                                                                         ^ >i+!">"+i=tnwwxxzyxgx|,          
+                                                                           `oC*pz7sxxwGmzyxe7Sws            
+                                                                         ^`.x*pPewZPyk&Y*nxuzx=}            
+                                                   ;+"}"|.             `.,]wu)}(Zu+}-i]n*i+}..,;,,`         
+                                                   |)(_~|.,.         ^.,"}>;}nSo>..~"}~:^-(,.,,,,,-         
+                                                 ;i=(+(s](ovi!>``   `,~7zw4Dk+~>_!inUx).`)-.,|~,,,-         
+                                                    !ul[Cv)?i",^`.^`.~=w|m@sv]vi+":,G7  `_".-!},,,|,.       
+                                                 .!z7>~"s*wCZxxxl(+)}vx_|Q9-~"|,,..|(!,,;-y"}}",,,!w"       
+                                           `..,,,-:>!!!}}+ixhhxUUy}[>"ii++_,|}zZwppz+}!!!}u}!i}"(xK0y.      
+                                         ^;,:|:;;;;;,,;~!++iYkS*fx)Czix=y"+>iS7syxLhfup00G7s(Sm&RW0hS(      
+                                        ,;,,,;||||~;||~}||!=+)=Z2S*U%&Gz7lsxtsv(!,;-}i==xfzxi"!!_=&OG)      
+                                     `,,.,,;||||||-,,"!-;,,"i=i=z*hhgRQQRs====i}~||~"ili}--xi.^``lOw(`      
+                                   .,,.,,;:||~~||-;.,,!"-;;;->"!+viSh44hZv=i=========ii+ `xREp=,,bk"        
+                                ..,..,,;|||~~~||!",....,|>"_,,,~ii((ozeox*zxzeeezxzwO$kWpOQEKH@RmU+^        
+                            .,--;..,,;||~~~~~|:",,~|,.....,,,,"Si:|,`~Y?*&E@QMMMMQQH6=eQQQHH(iWQ@RKo.       
+                    ^..,,||>~|...,,;||~~--~|:-"||||)~].....,}v;"!;|xy!ixfW6bnL4KMM0=|_OQQQ@@P``"PQ@REe|     
+             `....,,...,,||,..,,;||~~~-~~|:-"">>>>>")e_};(};i""|;+"_"-"}yUCxxxxtPn~;-}RQQQ@@Q"```tRQH@Ex    
+        .,,,;....,,,,,|||..,,;||~~~~~~||~-">>>>__=})O(!ii+(!|"(xGf.|"",`-(i=vv!SQ4;||=MQQQ@@Q=````+MMQ@E"   
+        ;,,,,,,,;:|~>~;,,;:||~~~~~||||,,,_"~-|~_>!ix!-)s"")e4P%xi+|;|::|")iltt=b$7,>,4M@QQQHQ*```"HMQ@D!    
+        |||||~~>>-~|,;|||||||||:|:,.`     |}"})}+)+|>="-->_""}";,,,,,:"}+!"+woxGD&x(i@0EHQQQQQheOMMQ@p;     
+        ~~-~_>>~||||||||||;||,..            ` +G!)p^.`..,|~~-_"}-,,,,.,>>,^|oyUwShP4w4hG@@HHQQQQMQQD[,^     
+        ~~">|~||:;;,,;;,,,.`                   ` ;+^       ..,|-""~,,,,|>>-"-([z4OGx6EO4k4RKORD&gz+;.:~>|.` 
+        ,":~-|:|;,;.^^                                          .,|-~:,|_~>>~~-+]](o0m$KWwbx ` ,_~||!~""!--|
+        :,|-~|||;,.,;                                              `  `..;|;-__>|>!_2hW&&4wxs`  ,| ^||,:_"-~
+        ,:|:;||:;,,,"                                                   `-.._v"|.-~,|fzbkosCs+       ,|  ,^ 
+
+
+
+       
+        No bugs No Errors
+        
+        Miko bless you
+
+*/
+
+
+
+
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MikoMikoMi : MonoBehaviour
+{
+   // to do 
+}
